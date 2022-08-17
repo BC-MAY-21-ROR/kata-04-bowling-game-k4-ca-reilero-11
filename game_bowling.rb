1c5f2fa240b7c0b9d1653175fe36dc6d648664de
EduardoBriceno89 <eduardbrice@outlook.com>
2022-08-17 17:48:30 -0500

    Add some conditionals.
    co-authored-by: claunicole <claunicole.gf@gmail.com>
    co-authored-by: CalixtoLopez <calixto.lopez.santos@gmail.com>
    co-authored-by: fahida rios <fahidarios@gmail.com>

--- game_bowling.rb
+++ game_bowling.rb
@@ -1,21 +1,37 @@
 class Bowling
     def initialize
         @number_pins
-        @frames = [] 
+        @frames = []
+        @tries = []
+        @tries2 = [] 
     end
 
     def random
         @number_pins = rand(0..10)
         puts @number_pins
     end  
-    def frame
-        10.times do       
-            @frames << random
-        end
-        puts @frames.join('')
-    end
+    def tries
+        
+        10.times do     
+            @tries << random
+            count = 0
 
+            if @tries != 10    
+                @tries2 << random
+
+                if (@tries[count] + @tries2[count]) <= 10
+                    @tries2 << random
+                end
+                puts @tries2.join
+
+            else
+                @tries2 = 0
+            end
+            count += 1
+        end
+        puts @tries.join
+    end
 end
 
 
-Bowling.new.frame
+Bowling.new.tries
