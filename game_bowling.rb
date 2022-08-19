45eee06d03fff33833f1863156f16181e6d34edd
claunicole <claunicole.gf@gmail.com>
2022-08-18 20:26:47 -0400

    feat: Add conditionals for strike and spare, the score and the bonus work properly
    co-authored-by: CalixtoLopez <calixto.lopez.santos@gmail.com>
    co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>
    co-authored-by: fahida rios <fahidarios@gmail.com>

--- game_bowling.rb
+++ game_bowling.rb
@@ -6,55 +6,50 @@ class Bowling
         @tries = []
         @tries2 = [] 
     end
-
-    def tries
-        
-        10.times do   
+  
+    def pins
         @number_pins = rand(0..10)
-        #puts @number_pins
-        a = @number_pins
-
-        b = 10 -a
-        @number_pins2 = rand(0..b)
-        c = @number_pins2
-        
-            #puts random1
-            @tries << a
-
-            #puts random2
-            #puts random1 + random2
-           
-            @tries2 << c
-           
+        diference = 10 - @number_pins
+        @number_pins2 = rand(0..diference)
+    end     
+  
+    def tries
+        score = 0
+  
+        10.times do   
+            pins
+            @tries << @number_pins
+            @tries2 << @number_pins2
         end
-        contador = 0
-        10.times do 
-            @frames << (@tries[contador] + @tries2[contador]) 
-            contador = contador + 1 
-        end 
-        puts @tries 
-        puts "*" 
-        puts @tries2 
-        puts "*suma" 
-        puts @frames
-        contador2 = 0
-        10.times do
-            if contador2 == 0 
-                 puts 'contador == 0'
-                 puts @frames[contador2] 
-            # elsif contador2 > 0 and contador2 <= 9
-            #     puts 'contador > 0 and contador <= 9'
-            #  puts @frames[contador2] +tries[contador2+1] + @frames[contador2-1]
-            # else
-            #     puts @frames[contador2] + tries[contador2+1] + @frames[contador2-1]
-             end
-            
-          contador2 =contador2+1
-        end     
-    end
-   
-end
-
+        puts "Intento 1"
+        puts @tries
+        puts "Intento 2"
+        puts @tries2
+        puts "Score 1"
+        puts @tries[0] + @tries2[0]
+      
+        @tries.each_with_index do |tries, index| 
+            if @tries[index] == 10
+                puts "Puntaje con strike #{index + 1}"
+                puts @tries[index].to_i + @tries2[index].to_i + @tries[index + 1].to_i + @tries2[index + 1].to_i
+            else if @tries[index] + @tries2[index] == 10
+                    puts "Puntaje con spare #{index + 1}"
+                    puts @tries[index].to_i + @tries2[index].to_i + @tries[index + 1].to_i
+            else
+            puts "puntaje #{index + 1}" 
+            puts @tries[index] + @tries2[index] 
+            puts "****"
+            end
+        end
+        end
+    end 
+  end
+  
+  
+    
+  
+  
+  Bowling.new.tries
 
     
 
